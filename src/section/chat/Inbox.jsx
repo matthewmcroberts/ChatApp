147c524c3b6df5068469073e0073834abb1b70fd
Matthew <matthew.mcroberts22@gmail.com>
2025-01-12 12:11:38 -0500

    working on chat form

--- src/section/chat/Inbox.jsx
+++ src/section/chat/Inbox.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import User01 from "../../images/user/user-01.png"
-import { DotsThree } from '@phosphor-icons/react'
+import { DotsThree, LinkSimple, PaperPlaneTilt, Smiley } from '@phosphor-icons/react'
 
 export default function Inbox() {
   return (
@@ -33,9 +33,25 @@ export default function Inbox() {
 
         {/* Input */}
         <div className="sticky bottom-0 border-t border-stroke bg-white px-6 py-5 dark:border-strokedark dark:bg-boxdark">
+            <form className="flex items-center justify-between space-x-4.5">
+                <div className="relative w-full">
+                    <input type="text" placeholder="Type something here" className="h-13 w-full rounded-md border border-stroke bg-gray pl-5 pr-19 text-black placeholder-body outline-none focus:border-primary dark:border-strokedark dark:bg-boxdark-2 dark:text-white"/>
+                    
+                    <div className="absolute right-5 top-1/2 -translate-y-1/2 items-center justify-end space-x-4">
+                        <button className="hover:text-primary">
+                            <LinkSimple size={20} />
+                        </button>
+                        <button className="hover:text-primary">
+                            <Smiley size={20} />
+                        </button>
+                    </div>
+                </div>
 
+                <button className="flex items-center justify-center h-13 max-w-13 w-full rounded-md bg-primary text-white hover:bg-opacity-90">
+                    <PaperPlaneTilt size={24} weight="bold"/>
+                </button>
+            </form>
         </div>
-
     </div>
   )
 }
