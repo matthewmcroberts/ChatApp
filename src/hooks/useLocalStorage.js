bd3a457a060ae42f40853af06f058c6476069867
Matthew <matthew.mcroberts22@gmail.com>
2025-01-16 19:30:24 -0500

    working on dark mode

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -4,7 +4,7 @@ const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
         try {
             const item = window.localStorage.getItem(key);
-
+            
             return item ? JSON.parse(item) : initialValue;
         } catch(error) {
             console.log(error);
