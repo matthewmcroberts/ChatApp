e8b2d587c340405d78ec329ebfd34c4ab1de2eea
Matthew <matthew.mcroberts22@gmail.com>
2025-01-20 02:07:50 -0500

    working on login page

---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div className="border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark h-screen">
+            <div className="flex flex-wrap items-center h-full">
+                <div className="hidden w-full xl:block x:w-1/2">
+                    <div className="py-17.5 px-26 text-center">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
