3f43ffbbaad0586ddf2c4b8181cd87dcc958f1cc
matthewmcroberts <mm20captingoof@gmail.com>
2025-01-24 16:49:40 -0500

    working on login page

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Logo from '../../components/Logo'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
     return (
@@ -6,7 +8,15 @@ export default function Login() {
             <div className="flex flex-wrap items-center h-full">
                 <div className="hidden w-full xl:block x:w-1/2">
                     <div className="py-17.5 px-26 text-center">
-                        
+                        <Link className="mb-5.5 inline-block">
+                            <Logo />
+                        </Link>
+
+                        <p className="px-20">
+                            Hey there, welcome back! Login to chat with your friends.
+                        </p>
+
+                        <img src="" alt="" />
                     </div>
                 </div>
             </div>
